e509351460c68b6e5260f953068a6d811155cb6b
shaw <shawwian@gmail.com>
2022-07-13 22:38:40 -0400

    22 part two

--- aoc2021/22-reactor-reboot/reboot.c
+++ aoc2021/22-reactor-reboot/reboot.c
@@ -2,12 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <inttypes.h>
 #include <string.h>
 #include <stdbool.h>
 
 #define MAX_CUBOIDS 65536
 #define MAX(a,b) (a > b ? a : b)
 #define MIN(a,b) (a < b ? a : b)
+
+/*      UINT64_MAX 18446744073709551615ULL */
+#define P10_UINT64 10000000000000000000ULL   /* 19 zeroes */
+#define E10_UINT64 19
+
+#define STRINGIZER(x)   # x
+#define TO_STRING(x)    STRINGIZER(x)
+
+
+typedef unsigned __int128 uint128_t; 
 
 typedef struct  {
     int min, max;
@@ -34,26 +45,34 @@ void part_one_naive(Cuboid steps[], int step_count) {
         for (int y=MAX(step.y.min, -50); y<=MIN(step.y.max, 50); ++y)
         for (int z=MAX(step.z.min, -50); z<=MIN(step.z.max, 50); ++z)
             grid[x+50][y+50][z+50] = step.on;
-
-
-        int count = 0;
-        for (int x=0; x<101; ++x)
-        for (int y=0; y<101; ++y)
-        for (int z=0; z<101; ++z)
-            if (grid[x][y][z]) ++count;
-
-        printf("After step %d: %d cubes are turned on\n", i, count);
     }
 
-    /*int count = 0;*/
-    /*for (int x=0; x<101; ++x)*/
-    /*for (int y=0; y<101; ++y)*/
-    /*for (int z=0; z<101; ++z)*/
-        /*if (grid[x][y][z]) ++count;*/
+    int count = 0;
+    for (int x=0; x<101; ++x)
+    for (int y=0; y<101; ++y)
+    for (int z=0; z<101; ++z)
+        if (grid[x][y][z]) ++count;
 
-    /*printf("After initialization procedure %d cubes are turned on\n", count);*/
+    printf("After initialization procedure %d cubes are turned on\n", count);
 }
 
+/* 
+ * from user Jonathan Leffler
+ * https://stackoverflow.com/questions/11656241/how-to-print-uint128-t-number-using-gcc
+ */
+static int print_u128(uint128_t u128) {
+    int rc;
+    if (u128 > UINT64_MAX) {
+        uint128_t leading  = u128 / P10_UINT64;
+        uint64_t  trailing = u128 % P10_UINT64;
+        rc = print_u128(leading);
+        rc += printf("%." TO_STRING(E10_UINT64) PRIu64, trailing);
+    } else {
+        uint64_t u64 = u128;
+        rc = printf("%" PRIu64, u64);
+    }
+    return rc;
+}
 
 bool cuboids_intersect(Cuboid a, Cuboid b, Cuboid *intersection) {
     intersection->x.min = MAX(a.x.min, b.x.min);
@@ -136,15 +155,14 @@ void part_two(Cuboid steps[], int step_count) {
     Cuboid *on_cuboids = calloc(MAX_CUBOIDS, sizeof(Cuboid));
 
     memset(on_cuboids, 0, step_count * sizeof(Cuboid));
-    uint64_t on_cuboids_count = 0;
+    uint128_t on_cuboids_count = 0;
 
     Cuboid *cuboids_to_add = malloc(MAX_CUBOIDS * sizeof(Cuboid));
-    uint64_t cuboids_to_add_count;
+    uint128_t cuboids_to_add_count;
 
-    uint64_t cuboids_to_remove_count;
+    uint128_t cuboids_to_remove_count;
 
     for (int step_i=0; step_i<step_count; ++step_i) {
-    /*for (int i=0; i<2; ++i) {*/
         Cuboid step = steps[step_i];
         bool found_intersection = false;
         cuboids_to_add_count = 0;
@@ -174,7 +192,6 @@ void part_two(Cuboid steps[], int step_count) {
             }
         }
 
-        /*printf("to_remove:\n");*/
         if (found_intersection) {
             /* remove cuboids */
             int left = 0;
@@ -190,17 +207,13 @@ void part_two(Cuboid steps[], int step_count) {
                     continue;
                 }
 
-                /*print_cuboid(&on_cuboids[left]);*/
-
                 on_cuboids[left++] = on_cuboids[right--];
             }
             on_cuboids_count -= cuboids_to_remove_count;
 
             /* add new cuboids */
-            /*printf("to_add:\n");*/
             for (int i=0; i<cuboids_to_add_count; ++i) {
                 assert(on_cuboids_count < MAX_CUBOIDS);
-                /*print_cuboid(&cuboids_to_add[i]);*/
                 on_cuboids[on_cuboids_count++] = cuboids_to_add[i];
             }
 
@@ -208,34 +221,24 @@ void part_two(Cuboid steps[], int step_count) {
 
         /* add new step */
         if (step.on) {
-            /*printf("to_add:\n");*/
-            /*print_cuboid(&step);*/
             assert(cuboids_to_add_count < MAX_CUBOIDS);
             on_cuboids[on_cuboids_count++] = step;
         } 
-
-        uint64_t count = 0;
-        for (int i=0; i<on_cuboids_count; ++i) {
-            Cuboid c = on_cuboids[i];
-            /*print_cuboid(&c);*/
-            count += (c.x.max+1 - c.x.min) * (c.y.max+1 - c.y.min) * (c.z.max+1 - c.z.min);
-        }
-
-        printf("After step %d: %llu cubes are turned on\n", step_i, count);
     }
 
-    printf("Found %llu cuboids\n", on_cuboids_count);
-    uint64_t count = 0;
+    uint128_t count = 0;
     for (int i=0; i<on_cuboids_count; ++i) {
         Cuboid c = on_cuboids[i];
-        count += (c.x.max+1 - c.x.min) * (c.y.max+1 - c.y.min) * (c.z.max+1 - c.z.min);
+        count += (uint128_t)(c.x.max+1 - c.x.min) * (c.y.max+1 - c.y.min) * (c.z.max+1 - c.z.min);
     }
 
-    printf("After reboot sequence %llu cubes are turned on\n", count);
+    printf("After reboot sequence "); 
+    print_u128(count);
+    printf(" cubes are turned on\n");
 
     free(cuboids_to_add);
-
 }
+
 
 int main(void) {
     int step_count = 0;
@@ -244,13 +247,11 @@ int main(void) {
     size_t len = 0;
     ssize_t nread;
 
-    FILE *fp = fopen("small_input.txt", "r");
+    FILE *fp = fopen("input.txt", "r");
 
     /* pass one, get step count */
     while ((nread = getline(&line, &len, fp)) != -1)
         ++step_count;        
-
-    printf("found %d steps\n", step_count);
 
     /* pass two, get cuboids */
     Cuboid steps[step_count];
@@ -274,7 +275,6 @@ int main(void) {
     fclose(fp);
 
     part_one_naive(steps, step_count);
-    printf("\n\n");
     part_two(steps, step_count);
 
     return 0;
