d11c435e08d74f4a07f161e59a6f504499288d4b
shaw <shawwian@gmail.com>
2023-01-08 17:42:58 -0600

    day 13 distress signal part two

--- aoc2022/13-distress-signal/distress.c
+++ aoc2022/13-distress-signal/distress.c
@@ -15,10 +15,8 @@ typedef enum {
 typedef struct Element Element;
 struct Element {
     Etype type;
-    union {
-        int num;
-        Element *list; // dynamic array
-    } data;
+	int num;
+	Element *list; // dynamic array
 };
 
 void print_list(Element *list) {
@@ -26,9 +24,9 @@ void print_list(Element *list) {
 	for (int i=0; i<arrlen(list); ++i) {
 		Element e = list[i];
 		if (e.type == ETYPE_INT)
-			printf("%d, ", e.data.num);
+			printf("%d, ", e.num);
 		else
-			print_list(e.data.list);
+			print_list(e.list);
 	}
 	printf("], ");
 }
@@ -37,29 +35,29 @@ int compare_elements(Element left, Element right) {
 	int result = 0;
 
 	if (left.type == ETYPE_INT && right.type == ETYPE_INT) {
-		result = left.data.num - right.data.num;
+		result = left.num - right.num;
 
 	} else if (left.type == ETYPE_LIST && right.type == ETYPE_LIST) {
 		int i_left=0, i_right=0;
-		while (i_left < arrlen(left.data.list) && i_right < arrlen(right.data.list)) {
-			result = compare_elements(left.data.list[i_left], right.data.list[i_right]);
+		while (i_left < arrlen(left.list) && i_right < arrlen(right.list)) {
+			result = compare_elements(left.list[i_left], right.list[i_right]);
 			if (result != 0) break;
 			++i_left; ++i_right;
 		}
 		if (result == 0) {
-			result = arrlen(left.data.list) - arrlen(right.data.list);
+			result = arrlen(left.list) - arrlen(right.list);
 		}
 
 	} else { 
 		Element as_list = { .type = ETYPE_LIST };
 		if (left.type == ETYPE_LIST) {
-			arrput(as_list.data.list, right);
+			arrput(as_list.list, right);
 			result = compare_elements(left, as_list);
 		} else {
-			arrput(as_list.data.list, left);
+			arrput(as_list.list, left);
 			result = compare_elements(as_list, right);
 		}
-		arrfree(as_list.data.list);
+		arrfree(as_list.list);
 	}
 	return result;
 }
@@ -73,9 +71,9 @@ int qsort_compare_elements(const void *a, const void *b) {
 void part_one(Element packets) {
 	int pair_index = 1;
 	int sum = 0;
-	for (int i=0; i<arrlen(packets.data.list) - 1; i+=2) {
-		Element left = packets.data.list[i];		
-		Element right = packets.data.list[i+1];		
+	for (int i=0; i<arrlen(packets.list) - 1; i+=2) {
+		Element left = packets.list[i];		
+		Element right = packets.list[i+1];		
 		int result = compare_elements(left, right);
 		if (result <= 0) sum += pair_index;
 		++pair_index;
@@ -87,32 +85,32 @@ void part_two(Element packets) {
 	// add divider packets
 	Element divider_one = { .type = ETYPE_LIST };
 	Element d1_list = { .type = ETYPE_LIST };
-	Element d1_num = { .type = ETYPE_INT, .data = { 2 }};
-	arrput(d1_list.data.list, d1_num);
-	arrput(divider_one.data.list, d1_list);
+	Element d1_num = { .type = ETYPE_INT, .num = 2 };
+	arrput(d1_list.list, d1_num);
+	arrput(divider_one.list, d1_list);
 
 	Element divider_two = { .type = ETYPE_LIST };
 	Element d2_list = { .type = ETYPE_LIST };
-	Element d2_num = { .type = ETYPE_INT, .data = { 6 }};
-	arrput(d2_list.data.list, d2_num);
-	arrput(divider_two.data.list, d2_list);
+	Element d2_num = { .type = ETYPE_INT, .num = 6 };
+	arrput(d2_list.list, d2_num);
+	arrput(divider_two.list, d2_list);
 
-	arrput(packets.data.list, divider_one);
-	arrput(packets.data.list, divider_two);
+	arrput(packets.list, divider_one);
+	arrput(packets.list, divider_two);
 
 	// sort packets
-	qsort(packets.data.list, arrlen(packets.data.list), sizeof(Element), qsort_compare_elements);
+	qsort(packets.list, arrlen(packets.list), sizeof(Element), qsort_compare_elements);
 
 	// locate divider packets
 	int index_one=0, index_two=0; 
-	for (int i=0; i<arrlen(packets.data.list); ++i) {
-		Element p = packets.data.list[i];
-		if (arrlen(p.data.list) != 1) continue;
-		Element first = p.data.list[0];
-		if (first.type == ETYPE_LIST && arrlen(first.data.list) == 1) {
-			if (first.data.list[0].data.num == 2)
+	for (int i=0; i<arrlen(packets.list); ++i) {
+		Element p = packets.list[i];
+		if (arrlen(p.list) != 1) continue;
+		Element first = p.list[0];
+		if (first.type == ETYPE_LIST && arrlen(first.list) == 1) {
+			if (first.list[0].num == 2)
 				index_one = i+1;
-			if (first.data.list[0].data.num == 6)
+			if (first.list[0].num == 6)
 				index_two = i+1;
 		}
 	}
@@ -133,7 +131,7 @@ Element parse_element(char **line) {
 		e.type = ETYPE_LIST;
 		while (*(*line) != ']') {
 			Element child = parse_element(line);
-			arrput(e.data.list, child);
+			arrput(e.list, child);
 		}
 		++(*line); // eat closing ]
 
@@ -146,7 +144,7 @@ Element parse_element(char **line) {
 		// NOTE(shaw): atoi converts the initial portion of the string pointed
 		// to by nptr to int, meaning it will stop parsing the number when it
 		// reaches a non base 10 digit
-		e.data.num = atoi(start);
+		e.num = atoi(start);
 	}
 
 	return e;
@@ -159,7 +157,7 @@ Element parse_input(U8 *input) {
 		char *line = arb_chop_by_delimiter((char**)&input, "\n");
 		if (*line == '\0') continue;
 		Element current = parse_element(&line);
-		arrput(packets.data.list, current);
+		arrput(packets.list, current);
 	} while (*input != '\0');
 
 	return packets;
