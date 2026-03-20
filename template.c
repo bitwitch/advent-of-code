5967ea27aa292002620daa61d7d8f971fde7d71a
Shaw <shawwian@gmail.com>
2026-03-20 17:20:59 -0400

    update template.c

--- template.c
+++ template.c
@@ -1,49 +1,31 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <string.h>
+#include "..\common.c"
 
-#include "../../base/base_inc.h"
-#include "../../base/base_inc.c"
-
-void part_one(uint8_t *input) {
-    printf("part_one: %d\n", 696969);
+void part_one(char *file_data, U64 file_size) {
+	(void)file_data; 
+	(void) file_size;
 }
 
-void part_two(uint8_t *input) {
-    printf("part_two: %d\n", 696969);
+void part_two(char *file_data, U64 file_size) {
+	(void)file_data; 
+	(void) file_size;
 }
 
 int main(int argc, char **argv) {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s [input_filepath]\n", argv[0]);
+		exit(1);
+	}
 
-    if (argc < 2) {
-        fprintf(stderr, "Must supply filepath to puzzle input\n");
-        exit(1);
-    }
+	char *filename = argv[1];
+	
+	char *file_data;
+	U64 file_size;
+	if (!read_entire_file(filename, &file_data, &file_size)) {
+		fatal("failed to read file %s", filename);
+	}
 
-    FILE *fp;
-    uint8_t *file_data, *file_copy;
-    size_t file_size;
+	part_one(file_data, file_size);
+	part_two(file_data, file_size);
 
-    fp = fopen(argv[1], "r");
-    if (!fp) { perror("fopen"); exit(1); }
-
-    int ok = arb_read_entire_file(fp, &file_data, &file_size);
-    if (ok != ARB_READ_ENTIRE_FILE_OK) {
-        fprintf(stderr, "Error: arb_read_entire_file()\n");
-        fclose(fp);
-        exit(1);
-    }
-
-    fclose(fp);
-
-    file_copy = malloc(file_size);
-    if (!file_copy) { perror("malloc"); exit(1); }
-    memcpy(file_copy, file_data, file_size);
-
-    part_one(file_copy);
-    part_two(file_data);
-
-    return 0;
+	return 0;
 }
